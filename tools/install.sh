4149233cee82cc412e353ed0f4549d9dbd912724
Anthony Morehouse <99203809+acmorehouse@users.noreply.github.com>
2026-07-28 20:17:50 -0700

    Change default repository for Oh My Zsh installation

--- tools/install.sh
+++ tools/install.sh
@@ -70,7 +70,7 @@ fi
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 # Default settings
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-acmorehouse/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
